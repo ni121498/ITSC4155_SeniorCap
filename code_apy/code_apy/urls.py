86c9d95b2f487e4d09c111c42c6e68580ef80448
NateCummens <78138781+NateCummens@users.noreply.github.com>
2021-04-14 17:42:53 -0400

    Hello World on homepage

--- code_apy/code_apy/urls.py
+++ code_apy/code_apy/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include('mainSite.urls')),
+    path('home/', include('mainSite.urls')),
     path('admin/', admin.site.urls),
 ]
