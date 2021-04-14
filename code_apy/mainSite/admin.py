ccaec4be85b2385a8a6f60e3ea95b8b3ed00158e
NateCummens <78138781+NateCummens@users.noreply.github.com>
2021-04-14 18:13:30 -0400

    created models and added admin

--- code_apy/mainSite/admin.py
+++ code_apy/mainSite/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Lesson, Module
 # Register your models here.
+admin.site.register(Lesson)
+admin.site.register(Module)
--- code_apy/mainSite/models.py
+++ code_apy/mainSite/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Module(models.Model):
+    # question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    module_number = models.IntegerField(default=0)
+    
+    
+class Lesson(models.Model):
+    lesson_text = models.TextField()
+    lesson_code_text = models.TextField()
+    lesson_psudo_code = models.TextField()
+    lesson_plain_text = models.TextField()
+    
+    needs_psudo = models.BooleanField()
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
