33e924a356a57f81c0346747f0466657599438f7
NateCummens <78138781+NateCummens@users.noreply.github.com>
2021-04-16 12:10:29 -0400

    Added a User Signup

--- code_apy/mainSite/views.py
+++ code_apy/mainSite/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from .models import Module
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 
 def index(request):
@@ -21,3 +24,8 @@ def languages(request):
 
 def cheatsheet(request):
     return render(request, template_name='mainSite/cheatsheet.html')
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
